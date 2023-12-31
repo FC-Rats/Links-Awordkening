3acfb913b2a0431d243ff638f70f4293f1fb441e
widfleer <loelia.coutellier@gmail.com>
2023-12-31 14:29:51 +0100

    test to push

--- Includes/test.php
+++ Includes/test.php
@@ -13,11 +13,11 @@ $response = [];
 
 list($test, $jsonError) = User::getJsonData($_POST["test"]);
 
-$score1=new Score(); $score1->setIdUser(1);
+$score1 = new Score();
+$score1->setIdUser(1);
 $test->setScores([$score1, new Score()]);
 
 //print_r($test);
 
 $response['Test'] = $test;
 echo json_encode($response);
-?>
