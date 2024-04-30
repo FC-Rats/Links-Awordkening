3ff12b7b65582ade05774044614cd6215db96d26
widfleer <loelia.coutellier@gmail.com>
2024-04-30 22:20:29 +0200

    AcceptRefuseFriendRequest

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import { ContainerInfoGame } from './components/organisms/ContainerInfoGame';
 import { ComponentEndGame } from './components/molecules/ComponentEndGame';
+import AcceptRefuseFriendRequest from './components/molecules/AcceptRefuseFriendRequest';
+
 
 function App() {
   return (
     <div className="App">
       <ContainerInfoGame />
       <ComponentEndGame/>
+      <AcceptRefuseFriendRequest isAcceptingButton={true} friend={{"id": 3, "username": "Lolo"}}/>
     </div>
   );
 }
--- frontend/src/components/molecules/AcceptRefuseFriendRequest.tsx
+++ frontend/src/components/molecules/AcceptRefuseFriendRequest.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import '../../assets/css/AcceptRefuseFriendRequest.css';
 
-function AcceptRefuseFriendRequest(props: {isAcceptingButton: Boolean}) {
+function AcceptRefuseFriendRequest(props: {isAcceptingButton: Boolean, friend: {id: number, username: string}}) {
   return (
-    <Button onClick={(e) => console.log(e)} className={props.isAcceptingButton === true ? "acceptRefuseFriendRequest acceptRefuseFriendRequest-accept" : "acceptRefuseFriendRequest acceptRefuseFriendRequest-refuse"} variant="contained">{props.isAcceptingButton === true ? "Accepter" : "Refuser"}</Button>
+    <Button onClick={(e) => console.log(e, props.friend)} className={props.isAcceptingButton === true ? "acceptRefuseFriendRequest acceptRefuseFriendRequest-accept" : "acceptRefuseFriendRequest acceptRefuseFriendRequest-refuse"} variant="contained">{props.isAcceptingButton === true ? "Accepter" : "Refuser"}</Button>
   )
 }
 
