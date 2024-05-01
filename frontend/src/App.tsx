49cb1d2644ae3e19c4e85e56de55383529862d65
widfleer <loelia.coutellier@gmail.com>
2024-05-01 15:05:57 +0200

    GameType

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,17 +8,18 @@ import { ContainerInfoPlayer } from './components/organisms/ContainerInfoPlayer'
 import { ComponentListWords } from './components/molecules/ComponentListWords';
 import StateFriendRequest from './components/molecules/StateFriendRequest';
 import Stack from '@mui/material/Stack';
+import GameType from './components/molecules/GameType';
 
 
 function App() {
   return (
     <div className="App">
-      {/*       <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <ContainerInfoPlayer />
         <ContainerInfoGame />
       </div>
       <ComponentListWords />
-      <ComponentEndGame /> */}
+      <ComponentEndGame />
       <AcceptRefuseFriendRequest isAcceptingButton={true} friend={{ "id": 3, "username": "Lolo" }} />
       <SearchFriends />
       <Stack spacing={2}>
@@ -26,6 +27,10 @@ function App() {
         <StateFriendRequest friend={{ "id": 4, "username": "Lna" }} />
         <StateFriendRequest friend={{ "id": 5, "username": "Léo" }} />
         <StateFriendRequest friend={{ "id": 10, "username": "Keke" }} />
+      </Stack>
+      <Stack spacing={2} direction="row" flexWrap="wrap" justifyContent="center" alignItems="center">
+        < GameType type='solo' />
+        < GameType type='multi' />
       </Stack>
     </div>
   );
--- frontend/src/components/molecules/GameType.tsx
+++ frontend/src/components/molecules/GameType.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import '../../assets/css/GameType.css'
 
-function GameType() {
+interface GameTypeProps {
+  type: 'solo' | 'multi';
+}
+
+function GameType({ type }: GameTypeProps) {
   return (
-    <div>GameType</div>
+    <Card className="card-GameType" sx={{ display: 'flex', cursor: 'pointer', alignItems: 'center', justifyContent: 'center' }} onClick={(e) => console.log(e, type)}>
+      <CardContent sx={{ display: 'column', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 2 }}>
+        <Avatar className='avatar-GameType' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          {type === 'solo' ? <AccountCircleRoundedIcon /> : <PeopleAltRoundedIcon />}
+        </Avatar>
+        <Typography component="span" variant="h6">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
