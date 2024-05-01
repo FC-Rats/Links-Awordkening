e9e4a0f8949fefb4722718ba2c930a956a3de46d
Léo Dessertenne <leodesse2@gmail.com>
2024-05-01 19:12:02 +0200

    Input form

--- frontend/src/components/molecules/InputForm.tsx
+++ frontend/src/components/molecules/InputForm.tsx
@@ -1,21 +1,61 @@
-import React from "react";
+import TextField from "@mui/material/TextField/TextField";
+import React, { useState } from "react";
+import "../../assets/css/InputForm.css"
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 interface InputFormProps {
     isAPasswordInput?: boolean; // Prop facultative avec ? avant le :
     value?: string;
     setSearch: (value: string) => void;
+    label: string;
+    required?: boolean;
 }
 
-export const InputForm = ({ isAPasswordInput, value, setSearch }: InputFormProps) => {
+export const InputForm = ({ isAPasswordInput, value, setSearch, label, required }: InputFormProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleTogglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+
     return (
         <div>
-            
-            {/* Utilisation de la prop isAPasswordInput pour déterminer le type de l'input */}
             {/*TODO: style, image, font */}
             {isAPasswordInput ? (
-                <input type="password"/>
+                <TextField
+                label={label}
+                className="input-form" 
+                type={showPassword ? "text" : "password"}
+                variant="outlined"
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                onClick={handleTogglePasswordVisibility}
+                                onMouseDown={(e) => e.preventDefault()}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
             ) : (
-                <input type="text" value={value ? value : ""} onChange={(e) => setSearch(e.target.value)}/>
+                <>
+                    <TextField 
+                        className="input-form" 
+                        label={required ? `${label}*` : label} 
+                        type="text" 
+                        variant="outlined" 
+                        value={value ? value : ""} 
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                </>
             )}
         </div>
     );
