ad975fa9a8ca827e174b8f18f6d30c5090b56503
Héléna Chevalier <lna.chevalier@gmail.com>
2024-05-04 22:16:57 +0200

    logo et title head

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Titre des pages et l'icone
+document.title = "Links Awordkening";
+const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+(link as HTMLLinkElement).type = 'image/x-icon';
+(link as HTMLLinkElement).rel = 'icon';
+(link as HTMLLinkElement).href = '/img/iconeLA.png';
+
+document.head.appendChild(link);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
