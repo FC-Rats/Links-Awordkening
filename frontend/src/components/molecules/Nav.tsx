33a81902c9d6af6b4119b0a7c68c6630f90f90e9
Léo Dessertenne <leodesse2@gmail.com>
2024-05-07 16:11:46 +0200

    nav with drag

--- frontend/src/components/molecules/Nav.tsx
+++ frontend/src/components/molecules/Nav.tsx
@@ -3,17 +3,56 @@ import "../../assets/css/Nav.css";
 
 export const Nav = (props: {}) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isDragging, setIsDragging] = useState(false);
+    const [initialY, setInitialY] = useState<number>(0);
+    const [navTop, setNavTop] = useState<number>(20); // Position initiale du haut de la navbar
+    const navHeight = 150; // Hauteur de votre navbar en pixels
 
     const toggleNavBar = () => {
         setIsOpen(!isOpen);
     }
 
+    const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+        setIsDragging(true);
+        setInitialY(event.clientY);
+        document.body.style.userSelect = "none";
+
+    }
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (isDragging) {
+            const movementY = event.clientY - initialY;
+            let newNavTop = navTop + movementY;
+
+            // Limite supérieure
+            newNavTop = Math.max(newNavTop, 0);
+            
+            // Limite inférieure
+            const windHeight = window.innerHeight;
+            newNavTop = Math.min(newNavTop, windHeight - navHeight);
+
+            setNavTop(newNavTop);
+            setInitialY(event.clientY);
+        }
+    }
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+        document.body.style.userSelect = "auto";
+
+    }
+
+    const handleMouseLeave = () => {
+        setIsDragging(false);
+        document.body.style.userSelect = "auto";
+
+    }
+
     const handleNavLinkClick = () => {
         setIsOpen(false);
     }
-    
     return (
-    <nav className={isOpen ? "open": ""}>
+    <nav className={isOpen ? "open": ""} style={{ top: `${navTop}px` }} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave}>
         <div className="nav-content">
             <div className="toggle-btn" onClick={toggleNavBar}>
                 <i className='btn-plus'></i>
