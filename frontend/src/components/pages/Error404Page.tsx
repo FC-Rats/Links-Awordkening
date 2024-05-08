261e8585c5d3fecd833c77eb9cda843257623699
widfleer <loelia.coutellier@gmail.com>
2024-05-08 17:23:17 +0200

    404

--- frontend/src/components/pages/Error404Page.tsx
+++ frontend/src/components/pages/Error404Page.tsx
@@ -1,11 +1,12 @@
 import { Error404Template } from "../templates/Error404Template";
-
+import { useLocation} from 'react-router-dom';
 
 export const Error404Page = () => {
+    const location = useLocation();
 
     return (
        <>
-            <Error404Template url=""/>
+            <Error404Template url={location.pathname.toString()}/>
        </>
     );
 };
--- frontend/src/components/templates/Error404Template.tsx
+++ frontend/src/components/templates/Error404Template.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { CenteredLogo } from "../atoms/CenteredLogo";
 import Stack from "@mui/material/Stack/Stack";
+import { Link } from 'react-router-dom';
 
 
 export const Error404Template = (props:{url:string}) => {
@@ -10,7 +11,8 @@ export const Error404Template = (props:{url:string}) => {
         <Stack spacing={{ xs: 1, sm: 2 }} direction="column" flexWrap="wrap" justifyContent="center" alignItems="center">
             <CenteredLogo/>
             <p><b>404</b>. That's an error.</p>
-            <p>The requested URL /{props.url} was not found on this server.<br></br> That's all we know.</p>       
+            <p>The requested URL /{props.url} was not found on this server.<br></br> That's all we know.</p>      
+            <p>Get back to <Link to="/">Home page</Link>.</p> 
        </Stack>
        </>
     );
