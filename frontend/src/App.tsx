a278e006b87f73327fc1164721281d877e890675
Léo Dessertenne <leodesse2@gmail.com>
2024-05-08 08:12:24 +0200

    404

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Error404 } from './components/templates/Error404';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Error404 url={"fzoei"}/>
   );
 }
 
---
+++ frontend/src/components/templates/Error404.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { CenteredLogo } from "../atoms/CenteredLogo";
+import Stack from "@mui/material/Stack/Stack";
+
+
+export const Error404 = (props:{url:string}) => {
+
+    return (
+       <>
+        <Stack spacing={{ xs: 1, sm: 2 }} direction="column" flexWrap="wrap" justifyContent="center" alignItems="center">
+            <CenteredLogo/>
+            <p><b>404</b>. That’s an error.</p>
+            <p>The requested URL /{props.url} was not found on this server.<br></br> That’s all we know.</p>       
+       </Stack>
+       </>
+    );
+};
