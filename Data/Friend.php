6b22dd317d1eeec9a9dfea44ad10ec04892048e0
widfleer <loelia.coutellier@gmail.com>
2024-05-12 15:46:36 +0200

    class friend

--- Data/Friend.php
+++ Data/Friend.php
@@ -2,36 +2,32 @@
 if (!class_exists("Friend", false)) {
     class Friend extends JSONable
     {
-        protected $idUser1;
-        protected $idUser2;
-        protected $areFriends;
-        protected $isPending;
+        protected $id_user;
+        protected $id_friend;
+        protected $state;
         protected $users;
         protected $errorData;
 
         public function __construct()
         {
-            $this->idUser1 = 0;
-            $this->idUser2 = 0;
-            $this->areFriends = 0;
-            $this->isPending = 0;
+            $this->id_user = 0;
+            $this->id_friend = 0;
+            $this->state = 0;
             $this->users = [];
             $this->errorData = null;
         }
 
         // Gets
-        public function getIdUser1():int { return $this->idUser1; }
-        public function getIdUser2():int { return $this->idUser2; }
-        public function getAreFriends():int { return $this->areFriends; }
-        public function getIsPending():int { return $this->isPending; }
+        public function getId_user():int { return $this->id_user; }
+        public function getId_friend():int { return $this->id_friend; }
+        public function getState():int { return $this->state; }
         public function getUsers():array { return $this->users; }
         public function getErrorData() { return $this->errorData; }
 
         // Sets
-        public function setIdUser1(int $idUser1) { $this->idUser1 = $idUser1; }
-        public function setIdUser2(int $idUser2) { $this->idUser2 = $idUser2; }
-        public function setAreFriends(int $areFriends) { $this->areFriends = $areFriends; }
-        public function setIsPending(int $isPending) { $this->isPending = $isPending; }
+        public function setId_user(int $id_user) { $this->id_user = $id_user; }
+        public function setId_friend(int $id_friend) { $this->id_friend = $id_friend; }
+        public function setState(int $state) { $this->state = $state; }
         public function setUsers(array $users) { $this->users = $users; }
         public function setErrorData($errorData) { $this->errorData = $errorData; }
     }
