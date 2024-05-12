b4c09d9fb55612ef6ff29e79c3cab34242688538
Héléna Chevalier <lna.chevalier@gmail.com>
2024-05-12 16:23:39 +0200

    code à tester sur une autre machine

--- api/Log/testClass.php
+++ api/Log/testClass.php
@@ -7,6 +7,7 @@ if (!class_exists('Connection')) {
 
 include('../Data/JSONable.php');
 include('../Data/Log.php');
+include '../Includes/core.php';
 
 $response = [];
 
--- frontend/src/components/templates/HomePageTemplate.tsx
+++ frontend/src/components/templates/HomePageTemplate.tsx
@@ -6,13 +6,30 @@ import Stack from "@mui/material/Stack/Stack";
 
 
 export const HomePageTemplate = () => {
-
+    
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+        const url = 'http://localhost:3000/Links-Awordkening/api/Log/testClass.php';
+        const data = {"idUser" : 145, "dateTime" : "11-11-1999 12:02:00", "log" : "Testing", "ip" : "12.0.0.2"};
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:  JSON.stringify({ log : data })
+        });
+        const rep = await response.json();
+        console.log(rep);
+    };
 
     return (
         <>
             <Stack spacing={5} direction="column" flexWrap="wrap" justifyContent="center" alignItems="center">
                 <CenteredLogo />
-                <SubmitButton text={"Jouer"} />
+                <form method="post" onSubmit={handleSubmit}>
+                    <div style={{ textAlign: "center" }} >
+                    <SubmitButton text={"Jouer"} />
+                    </div>
+                </form>
                 {/* TODO changer par un RedirectButton quand les routeurs seront fait  */}
                 <CaseHomePageContainer />
             </Stack>
