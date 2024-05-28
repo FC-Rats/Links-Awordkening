04089617f2e7ed7bc894e3ba2e41d07b30f83570
Léo Dessertenne <leodesse2@gmail.com>
2024-05-28 23:20:07 +0200

    SAELA-295   Amélioration composant HomePage Podium

--- frontend/src/components/molecules/CaseHomePage.tsx
+++ frontend/src/components/molecules/CaseHomePage.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import "../../assets/css/CaseHomePage.css";
 
 export const CaseHomePage = (props: { title: string; pseudo: string; value: string; color: string; }) => {
+    const renderIcon = () => {
+        switch (props.title) {
+            case '#1':
+                return "/img/icons/gold.png";
+            case '#2':
+                return "/img/icons/silver.png";
+            case '#3':
+                return "/img/icons/bronze.png";
+            default:
+                return "";
+        }
+    };
     return (
-        <div className="case-home-page" style={{ backgroundColor: props.color }}>
-            <p className="title-case">{props.title}</p>
+        <div className="case-home-page" style={{ backgroundColor: props.color }} >
+            {renderIcon() && <img src={renderIcon()} alt="icon" className="title-icon" />}
             <p className="title-case">{props.pseudo}</p>
             <h2 className="value-case">{props.value}</h2>
         </div>
--- frontend/src/components/organisms/CaseHomePageContainer.tsx
+++ frontend/src/components/organisms/CaseHomePageContainer.tsx
@@ -16,14 +16,21 @@ export const CaseHomePageContainer = ({ scores }: { scores: Score[]}) => {
         <>
         <h2>Meilleurs Joueurs</h2>
         <Stack spacing={2} direction="row" flexWrap="wrap" justifyContent="center" alignItems="center">
-            {scores.length > 0 && scores[0] && (
-                <CaseHomePage title={"#1"} pseudo={context?.user?.name === scores[0].username || scores[0].visibility === 'PUBLIC'  ? scores[0].username : "PROFIL PRIVÉ" } value={scores[0].totalScore} color={'var(--asparagus)'} />
-            )}
+
             {scores.length > 1 && scores[1] && (
-                <CaseHomePage title={"#2"} pseudo={context?.user?.name === scores[1].username || scores[1].visibility === 'PUBLIC' ? scores[1].username : "PROFIL PRIVÉ"} value={scores[1].totalScore} color={'var(--coffee)'} />
+                <div style={{ paddingBottom: '75px' }}>
+                    <CaseHomePage title={"#2"} pseudo={context?.user?.name === scores[1].username || scores[1].visibility === 'PUBLIC' ? scores[1].username : "PROFIL PRIVÉ"} value={scores[1].totalScore} color={'var(--hunterGreen)'} />
+                </div>
+            )}
+            {scores.length > 0 && scores[0] && (
+                <div style={{ paddingBottom: '125px' }}>
+                    <CaseHomePage title={"#1"} pseudo={context?.user?.name === scores[0].username || scores[0].visibility === 'PUBLIC'  ? scores[0].username : "PROFIL PRIVÉ" } value={scores[0].totalScore} color={'var(--asparagus)'} />
+                </div>
             )}
             {scores.length > 2 && scores[2] && (
-                <CaseHomePage title={"#3"} pseudo={context?.user?.name === scores[2].username || scores[2].visibility === 'PUBLIC' ? scores[2].username : "PROFIL PRIVÉ"} value={scores[2].totalScore} color={'var(--hunterGreen)'} />
+                <div>
+                    <CaseHomePage title={"#3"} pseudo={context?.user?.name === scores[2].username || scores[2].visibility === 'PUBLIC' ? scores[2].username : "PROFIL PRIVÉ"} value={scores[2].totalScore} color={'var(--coffee)'} />
+                </div>
             )}
         </Stack>
         </>
