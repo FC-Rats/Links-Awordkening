4a0dcfaeb3988262044f96e432e0b8f6e16492b0
TheIceMagmaCube <theicemagmacube.contact@gmail.com>
2024-05-29 20:41:51 +0200

    Update init.sh
    A

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
-# Démarre l'environnement python (inside Docker)
-# Celui qui touche : Pied > Bouche
-# Amicalement.
+echo WOOFERSCRIPT
+echo By BlitzWoof!
+echo Linkawordkening Startup
+echo ---
+echo Starting Apache stuff...
+apache2ctl restart
+echo Apache initialized
+echo Booting PythonENV
 cd /var/www/html/Game/server/
 python main.py
-
-# N'EST PAS PART DU INIT SH
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# pip install websockets
-# cd /var/www/html/Game/server/
-# python3 main.py
