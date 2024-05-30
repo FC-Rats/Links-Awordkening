08520c9e468daa8ec0750d6a7f226e175efb46be
Léo Dessertenne <leodesse2@gmail.com>
2024-05-30 13:08:22 +0200

    podium

--- frontend/src/components/molecules/CaseHomePage.tsx
+++ frontend/src/components/molecules/CaseHomePage.tsx
@@ -4,18 +4,18 @@ import "../../assets/css/CaseHomePage.css";
 export const CaseHomePage = (props: { title: string; pseudo: string; value: string; color: string; }) => {
     const renderIcon = () => {
         switch (props.title) {
-            case '#1':
+            case '1':
                 return "/img/icons/gold.png";
-            case '#2':
+            case '2':
                 return "/img/icons/silver.png";
-            case '#3':
+            case '3':
                 return "/img/icons/bronze.png";
             default:
                 return "";
         }
     };
     return (
-        <div className="case-home-page" style={{ backgroundColor: props.color }} >
+        <div className="case-home-page" style={{ backgroundColor: props.color }} id={"id"+props.title}>
             {renderIcon() && <img src={renderIcon()} alt="icon" className="title-icon" />}
             <p className="title-case">{props.pseudo}</p>
             <h2 className="value-case">{props.value}</h2>
--- frontend/src/components/organisms/CaseHomePageContainer.tsx
+++ frontend/src/components/organisms/CaseHomePageContainer.tsx
@@ -3,6 +3,7 @@ import "../../assets/css/CaseHomePage.css";
 import Stack from "@mui/material/Stack/Stack";
 import { CaseHomePage } from "../molecules/CaseHomePage";
 import { AppContext } from "../hooks/AppContext";
+import Typography from "@mui/material/Typography/Typography";
 
 interface Score {
     username: string;
@@ -14,23 +15,17 @@ export const CaseHomePageContainer = ({ scores }: { scores: Score[]}) => {
     const context = useContext(AppContext);
     return (
         <>
-        <h2>Meilleurs Joueurs</h2>
-        <Stack spacing={2} direction="row" flexWrap="wrap" justifyContent="center" alignItems="center">
+        <Typography variant="h4">Meilleurs Joueurs</Typography>
+        <Stack spacing={2} direction="row" flexWrap="wrap" justifyContent="center" alignItems="flex-end">
 
             {scores.length > 1 && scores[1] && (
-                <div style={{ paddingBottom: '75px' }}>
-                    <CaseHomePage title={"#2"} pseudo={context?.user?.name === scores[1].username || scores[1].visibility === 'PUBLIC' ? scores[1].username : "PROFIL PRIVÉ"} value={scores[1].totalScore} color={'var(--hunterGreen)'} />
-                </div>
+                    <CaseHomePage title={"2"} pseudo={context?.user?.name === scores[1].username || scores[1].visibility === 'PUBLIC' ? scores[1].username : "PROFIL PRIVÉ"} value={scores[1].totalScore} color={'var(--hunterGreen)'} />
             )}
             {scores.length > 0 && scores[0] && (
-                <div style={{ paddingBottom: '125px' }}>
-                    <CaseHomePage title={"#1"} pseudo={context?.user?.name === scores[0].username || scores[0].visibility === 'PUBLIC'  ? scores[0].username : "PROFIL PRIVÉ" } value={scores[0].totalScore} color={'var(--asparagus)'} />
-                </div>
+                    <CaseHomePage title={"1"} pseudo={context?.user?.name === scores[0].username || scores[0].visibility === 'PUBLIC'  ? scores[0].username : "PROFIL PRIVÉ" } value={scores[0].totalScore} color={'var(--asparagus)'} />
             )}
             {scores.length > 2 && scores[2] && (
-                <div>
-                    <CaseHomePage title={"#3"} pseudo={context?.user?.name === scores[2].username || scores[2].visibility === 'PUBLIC' ? scores[2].username : "PROFIL PRIVÉ"} value={scores[2].totalScore} color={'var(--coffee)'} />
-                </div>
+                    <CaseHomePage title={"3"} pseudo={context?.user?.name === scores[2].username || scores[2].visibility === 'PUBLIC' ? scores[2].username : "PROFIL PRIVÉ"} value={scores[2].totalScore} color={'var(--coffee)'} />
             )}
         </Stack>
         </>
