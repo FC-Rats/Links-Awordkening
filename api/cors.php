ba7abe1a44b6c4831896533cd9c937fcc6ec5de3
widfleer <loelia.coutellier@gmail.com>
2024-06-03 15:22:58 +0200

    CORS to test

--- api/cors.php
+++ api/cors.php
@@ -1,11 +1,11 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *'); // on fabien's server : https://linksawordkening.fabiengilles.tf
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: X-Requested-With, Authorization, Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: *'); // on fabien's server : https://linksawordkening.fabiengilles.tf
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
     header('Access-Control-Allow-Headers: token, Content-Type, Authorization'); 
     header('Access-Control-Max-Age: 1728000');
